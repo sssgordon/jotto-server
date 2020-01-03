e046571533361f3a49401b7e85e2c5bdf8d6f274
sssgordon <gso7@uwo.ca>
2020-01-03 20:54:05 +0100

    Initial commit

--- random-word-server/server.js
+++ random-word-server/server.js
@@ -1,31 +1,28 @@
-const express = require("express");
-const cors = require("cors");
-const fs = require("fs");
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
 
 const app = express();
-const port = process.env.PORT || 3030;
 
-app.use(
-    cors({
-        origin: "http://obscure-fortress-88963.herokuapp.com/",
-        credentials: true
-    })
-);
+// CORS for react app, assuming port 3000
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}))
 
 // read words from json file
-const fileContents = fs.readFileSync("./five-letter-words.json", "utf-8");
+const fileContents = fs.readFileSync('./five-letter-words.json', 'utf-8');
 const words = JSON.parse(fileContents);
 const { fiveLetterWords } = words;
 
-app.get("/", (req, res) => {
-    // select a random word
-    const word =
-        fiveLetterWords[Math.floor(Math.random() * fiveLetterWords.length)];
+app.get('/', (req, res) => {
+  // select a random word
+  const word = fiveLetterWords[Math.floor(Math.random() * fiveLetterWords.length)]
 
-    // return it as the response
-    res.send(word);
-});
+  // return it as the response
+  res.send(word)
+})
 
-app.listen(port, () => console.log(`Word server listening on port ${port}!`));
+app.listen(3030, () => console.log('Word server listening on port 3030!'))
 
 module.exports = app;
